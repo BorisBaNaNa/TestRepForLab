e362f84bf365af0ed6ae2acecb7e0a1109e7ee0b
BorisBaNaNa <superboris2001@yandex.ru>
2022-06-14 12:11:55 +0300

    modified code 1

--- GitLab/GitLab.cpp
+++ GitLab/GitLab.cpp
@@ -1,10 +1,23 @@
 ﻿#include <iostream>
 
 using namespace std;
+
+class User {
+    string name;
+
+public:
+    User(string name_) : name(name_) {}
+
+    string GetUserName() {
+        return name;
+    }
+};
+
 int main()
 {
     cout << "Hello, user, what is you name?\n";
     string Name;
     cin >> Name;
-    cout << "Hello, " << Name << "!";
+    User user(Name);
+    cout << "Hello, " << user.GetUserName() << "!";
 }
