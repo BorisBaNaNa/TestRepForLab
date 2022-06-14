5d8998f1db39db6ad611ae6b01fa090ea320bfff
BorisBaNaNa <superboris2001@yandex.ru>
2022-06-14 11:32:54 +0300

    modified code

--- GitLab/GitLab.cpp
+++ GitLab/GitLab.cpp
@@ -1,6 +1,10 @@
 ﻿#include <iostream>
 
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello, user, what is you name?\n";
+    string name;
+    cin >> name;
+    cout << "Hello, " << name << "!";
 }
